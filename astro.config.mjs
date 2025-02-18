16120e288f648e2920e792c3e1b172eeb2a52e26
Jairo Sepulveda C <30697670+infraestructura-it@users.noreply.github.com>
2025-02-17 21:23:37 -0500

    servicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+    site:'https://github.io/infraestructura-it/webcore-astro-portfolio',
     vite: {
         ssr: {
             noExternal: ['webcoreui']
